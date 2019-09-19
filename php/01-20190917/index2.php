e459f5d0c5cc55429c763967b87a3269247a1ac7
Trong Nhan Nguyen <tnnguyen8787@gmail.com>
2019-09-19 18:02:46 +0700

    uploaded

---
+++ php/01-20190917/index2.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+</head>
+<body>
+    <h1>This is a test</h1>
+    <?php
+    echo "Testing PHP normal";
+    echo "<h2> Testing with bold h2</h2>";
+    ?>
+</body>
+</html>
